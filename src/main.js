b636eddf0eef8cf1c112e04e14295e98f9169ea2
Sam Stenvall <sam.stenvall@nordsoftware.com>
2019-05-30 13:26:26 +0300

    Move the "routing" into the App component

--- src/main.js
+++ src/main.js
@@ -3,13 +3,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './components/app/app';
-import { Home } from './components/home/home';
-import { NotFound } from './components/not-found/not-found';
 import './main.scss';
 
 render(
-  <App>
-    {window.location.pathname === '/' ? <Home /> : <NotFound />}
-  </App>,
+  <App />,
   document.getElementById('root')
 );
