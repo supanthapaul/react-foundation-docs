3632f1eaa3dbc0ad6b9f538257ea0bdee03b1222
Supantha Paul <codewithharsh@gmail.com>
2020-10-01 13:28:50 +0530

    Fixed Page not Found on sub-directories

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,9 +3,13 @@ import { Navbar } from '../navbar/navbar';
 import { Home } from '../home/home';
 import { NotFound } from '../not-found/not-found';
 
-export const App = () => (
-  <div className="app">
-    <Navbar />
-    {window.location.pathname === '/' ? <Home /> : <NotFound />}
-  </div>
-);
+export const App = () => {
+	const allowedPaths = ['/', '/react-foundation-docs/', '/react-foundation-docs'];
+
+	return (
+		<div className="app">
+			<Navbar />
+			{allowedPaths.includes(window.location.pathname) ? <Home /> : <NotFound />}
+		</div>
+	)
+};
