d04aa85623a11502f1cadf2303085e14bfe6dc93
Supantha Paul <codewithharsh@gmail.com>
2020-10-01 13:35:38 +0530

    App.js file linted

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,12 +4,12 @@ import { Home } from '../home/home';
 import { NotFound } from '../not-found/not-found';
 
 export const App = () => {
-	const allowedPaths = ['/', '/react-foundation-docs/', '/react-foundation-docs'];
+  const allowedPaths = ['/', '/react-foundation-docs/', '/react-foundation-docs'];
 
-	return (
-		<div className="app">
-			<Navbar />
-			{allowedPaths.includes(window.location.pathname) ? <Home /> : <NotFound />}
-		</div>
-	)
+  return (
+    <div className="app">
+      <Navbar />
+      {allowedPaths.includes(window.location.pathname) ? <Home /> : <NotFound />}
+    </div>
+  );
 };
